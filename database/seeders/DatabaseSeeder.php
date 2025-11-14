99ee2b1e339d7ce8ba3b82debe4a63907d83e76b
asmi046 <asmi046@gmail.com>
2025-11-14 18:02:41 +0300

    Шапка

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call(
+            [
+                SeoDataSeeder::class
+            ]
+        );
 
         User::factory()->create([
             'name' => 'Test User',
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,5 +10,18 @@
 
 @section('main')
 
+    <header class="hero-header" id="hero-header">
+        <video class="hero-header__video" src="{{ asset('video/pili-video.mp4') }}" autoplay muted loop playsinline></video>
+        <div class="hero-header__overlay"></div>
+        <div class="container">
+            <div class="hero-header__content">
+                <img class="hero-header__logo" src="{{ asset('img/logo.svg') }}" alt="Holsfaller">
+                <p class="hero-header__tagline">Профессиональные ленточные пилы <br>Европейского качества</p>
+                <a class="hero-header__phone" href="tel:+79192707805">+7 919 270-78-05</a>
+                <a class="button hero-header__cta" href="#order">Заказать</a>
+            </div>
+        </div>
+    </header>
+
 @endsection
 
